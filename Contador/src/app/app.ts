19c4f485ef58572c613569ccfda07f6bd8f591bb
rauliesan <raullsdev@gmail.com>
2026-01-08 11:00:31 +0100

    Proyecto Contador

---
+++ Contador/src/app/app.ts
@@ -0,0 +1,21 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('Contador');
+  contador = 1;
+
+  incrementar(){
+    this.contador++;
+  }
+
+  decrementar(){
+    this.contador--;
+  }
+}
